b5ca836a19e0c2f19823b86ec6d694a0cc082770
Lansarbac2020 <secretariat.lansarcenter@gmail.com>
2025-01-11 12:31:47 +0300

    update doctors appointtment views

--- admin/src/components/SideBar.jsx
+++ admin/src/components/SideBar.jsx
@@ -22,7 +22,7 @@ const SideBar = () => {
   }
   to={'/admin-dashbard'}
 >
-  <img src={assets.home_icon} alt='' />Dashboard
+  <img src={assets.home_icon} alt='' /><  p  className='hidden md:block'>Dashboard</p>
 </NavLink>
 
                 <NavLink
@@ -31,7 +31,8 @@ const SideBar = () => {
                       isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
                     }`
                   }
-                to={'/all-appointments'}><img src={assets.appointment_icon} alt=''/>Appointment</NavLink>
+                to={'/all-appointments'}><img src={assets.appointment_icon} alt=''/>
+                <p className='hidden md:block'>Appointment</p></NavLink>
                 <NavLink
                 className={({ isActive }) =>
                     `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
@@ -39,7 +40,7 @@ const SideBar = () => {
                     }`
                   }
                 
-                to={'/add-doctor'}><img src={assets.add_icon} alt=''/>Add Doctor</NavLink>
+                to={'/add-doctor'}><img src={assets.add_icon} alt=''/> <p className='hidden md:block'>Add Doctor</p></NavLink>
                 <NavLink 
                 
                 className={({ isActive }) =>
@@ -47,7 +48,7 @@ const SideBar = () => {
                       isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
                     }`
                   }
-                to={'/doctor-list'}><img src={assets.people_icon} alt=''/>Doctors List</NavLink>
+                to={'/doctor-list'}><img src={assets.people_icon} alt=''/><p className='hidden md:block'>Doctors List</p> </NavLink>
                 
             </ul>
         }
